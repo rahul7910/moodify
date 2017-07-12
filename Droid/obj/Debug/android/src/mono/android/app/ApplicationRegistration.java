109deef04dae24bda466433e8d3f09e47bcc7fd5
Rahul <c.ronaldo9@icloud.com>
2017-07-12 12:09:57 +1200

    Add project files and folders

--- Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ Droid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,7 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
-		mono.android.Runtime.register ("App2.Droid.MainApplication, App2.Droid, Version=1.0.6399.35557, Culture=neutral, PublicKeyToken=null", md53a8ac554aa1b14bea1e0e8409935082a.MainApplication.class, md53a8ac554aa1b14bea1e0e8409935082a.MainApplication.__md_methods);
+		mono.android.Runtime.register ("App2.Droid.MainApplication, App2.Droid, Version=1.0.6399.42313, Culture=neutral, PublicKeyToken=null", md53217ad6e428268a90ce9301cb1d879cf.MainApplication.class, md53217ad6e428268a90ce9301cb1d879cf.MainApplication.__md_methods);
 		
 	}
 }
